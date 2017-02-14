ea8347243a996e22a76772114f955c24eec20439
Ryan Murphy <ryanmurphy@gatech.edu>
2017-02-14 15:07:27 -0500

    Added logout feature

--- app/src/main/java/edu/gatech/cleanwater/controllers/TempActivity.java
+++ app/src/main/java/edu/gatech/cleanwater/controllers/TempActivity.java
@@ -1,7 +1,10 @@
 package edu.gatech.cleanwater.controllers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import edu.gatech.cleanwater.R;
 
@@ -11,5 +14,15 @@ public class TempActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp);
+
+        final Button bLogout = (Button) findViewById(R.id.bLogout);
+
+        bLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent logout = new Intent(TempActivity.this, WelcomeActivity.class);
+                TempActivity.this.startActivity(logout);
+            }
+        });
     }
 }
