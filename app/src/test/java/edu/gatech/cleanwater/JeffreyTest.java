3b5c2d9fbad037d6ad92687d76e4517ec7826bc6
Jeffrey Lin <jlin329@gatech.edu>
2017-04-05 21:00:21 -0400

    Added registerUser test

---
+++ app/src/test/java/edu/gatech/cleanwater/JeffreyTest.java
@@ -0,0 +1,40 @@
+package edu.gatech.cleanwater;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.gatech.cleanwater.Model.FirebaseHelper;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * Created by Jeffrey on 4/5/2017.
+ */
+
+public class JeffreyTest {
+
+    private String user;
+    private String pass;
+    private String empty;
+
+    @Before
+    public void setup() {
+        user = "john smith";
+        pass = "password";
+        empty = "";
+    }
+
+    @Test
+    public void testRegisterUser() {
+        assertEquals("good", FirebaseHelper.registerUser(user, pass));
+
+        assertEquals("bad username", FirebaseHelper.registerUser(empty, pass));
+
+        assertEquals("bad password", FirebaseHelper.registerUser(user, empty));
+
+        assertEquals("bad username", FirebaseHelper.registerUser(empty, empty));
+    }
+}
